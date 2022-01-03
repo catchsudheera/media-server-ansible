cf874cfad86e0ebd522b9910dadef85f99367808
sudheera <catchsudheera@gmail.com>
2022-01-03 13:42:45 +0100

    Added jackett,sonarr,radarr config files with templates

---
+++ media-server-ansible/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo " - Prepare for installation"
+if ansible-playbook site.yml -i inventories/default/hosts.ini --tags stop-stacks  ; then
+    echo "  -- Stacks stopped"
+else
+    echo ""
+    echo "+++++++++++++++++++++++++++++++++++    ERROR    +++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "There are running docker containers which did not stop. Check above ansible errors or try running this script again..."
+    echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo ""
+    exit 1
+fi
+
+
+echo ""
+echo "--------------------------------------------------------------"
+echo " - Start installation"
+ansible-playbook site.yml -i inventories/default/hosts.ini
+
