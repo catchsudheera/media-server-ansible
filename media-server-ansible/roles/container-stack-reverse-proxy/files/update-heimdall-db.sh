a57b753b3a497e05ef27cfa585b4d140a38e615f
sudheera <catchsudheera@gmail.com>
2022-01-03 23:42:48 +0100

    Added subdomain based sub-sites

--- media-server-ansible/roles/container-stack-reverse-proxy/files/update-heimdall-db.sh
+++ media-server-ansible/roles/container-stack-reverse-proxy/files/update-heimdall-db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sqlite3 "$1"/www/app.sqlite <<< "update items set url='https://$2/$3' where url = 'my-url' and title = '$3' COLLATE NOCASE"
+sqlite3 "$1"/www/app.sqlite <<< "update items set url='https://$3.$2' where url = 'my-url' and title = '$3' COLLATE NOCASE"
