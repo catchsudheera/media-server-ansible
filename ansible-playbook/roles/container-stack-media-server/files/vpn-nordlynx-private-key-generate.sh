156f3a30bbdc39611afe867e9112ea758790c2eb
Sudheera Palihakkara <catchsudheera@gmail.com>
2022-05-24 19:03:28 +0200

    Update vpn-nordlynx-private-key-generate.sh

--- ansible-playbook/roles/container-stack-media-server/files/vpn-nordlynx-private-key-generate.sh
+++ ansible-playbook/roles/container-stack-media-server/files/vpn-nordlynx-private-key-generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --cap-add=NET_ADMIN -e USER="$1" -e PASS="$2" bubuntux/nordvpn nord_private_key 2>/dev/null | grep "Private Key: " | sed 's/Private Key: //'
+docker run --rm --cap-add=NET_ADMIN -e USER="$1" -e PASS="$2" bubuntux/nordvpn:get_private_key 2>/dev/null | grep "Private Key: " | sed 's/Private Key: //'
